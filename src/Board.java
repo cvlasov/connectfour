df5a5e3954c43b01ad104f8b81323f09e8f82a7e
cvlasov <catherinev.canada@gmail.com>
2016-09-18 21:09:44 +0200

    Added graphics so that game is played in a window

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,5 @@
 package connectfour;
 
-import java.util.Arrays;
-
 import connectfour.GameHelper.Piece;
 
 /**
@@ -9,32 +7,35 @@ import connectfour.GameHelper.Piece;
  *    <p>
  *    It is initialized with 6 rows and 7 columns in the constructor such that:
  *    <ul>
- *    <li>Top left is {@code board[0][0]}</li>
- *    <li>Bottom left is {@code board[5][0]}</li>
- *    <li>Top right is {@code board[0][6]}</li>
- *    <li>Bottom right is {@code board[5][6]}</li>
+ *      <li>Top left is {@code board[0][0]}</li>
+ *      <li>Bottom left is {@code board[5][0]}</li>
+ *      <li>Top right is {@code board[0][6]}</li>
+ *      <li>Bottom right is {@code board[5][6]}</li>
  *    </ul>
  */
 public final class Board {
 	
-	private GameHelper.Piece[][] board;
+	private Piece[][] pieces;
 	
-	public Board () {
-		board = new GameHelper.Piece[6][7];
-		// Initialize all entries to NONE
-		Arrays.fill(board, Piece.NONE);
-	}
-	
-	public Piece[][] getBoard() {
-		return board;
+	/**
+	 * Initializes the board such that there are no pieces.
+	 */
+	public Board() {
+		pieces = new Piece[GameHelper.NUM_OF_ROWS][GameHelper.NUM_OF_COLS];
+		for (int row = 0; row < GameHelper.NUM_OF_ROWS; row++) {
+			for (int col = 0; col < GameHelper.NUM_OF_COLS; col++) {
+				pieces[row][col] = Piece.NONE; 
+			}
+		}
 	}
 	
 	/**
 	 * Returns whether or not placing a piece in the given column is valid
 	 */
 	public boolean isValidMove (int columnNum) {
-		// Check if the top of the column is empty
-		return this.board[0][columnNum] == Piece.NONE;
+		return columnNum > -1
+				&& columnNum < GameHelper.NUM_OF_COLS
+				&& this.pieces[0][columnNum] == Piece.NONE;
 	}
 	
 	/**
@@ -42,55 +43,69 @@ public final class Board {
 	 * <p>
 	 * <b>Note</b>: requires the move to be valid
 	 */
-	public int placePiece(int columnNum, GameHelper.Piece pieceType) {
+	public int placePiece(int columnNum, Piece pieceType) {
 		// Start at the bottom and find the first empty position
-		for (int rowNum = 5; rowNum >= 0; rowNum--) {
-			if (board[rowNum][columnNum] == Piece.NONE) {
-				board[rowNum][columnNum] = pieceType;
+		for (int rowNum = GameHelper.NUM_OF_ROWS - 1; rowNum >= 0; rowNum--) {
+			if (pieces[rowNum][columnNum] == Piece.NONE) {
+				pieces[rowNum][columnNum] = pieceType;
 				return rowNum;
 			}
 		}
-		// Fail
 		return -1;
 	}
 	
 	/**
-	 * Checks if the most recent piece in the given column resulted in a win
+	 * Checks if the piece in the given row and column resulted in a win.
 	 */
-	public boolean hasWon (int pieceColumn) {
-		
-		// Get row of the most recent piece in this column
-		int pieceRow = 0;
-		for (int rowNum = 0; rowNum < 6; rowNum++) {
-			if (board[rowNum][pieceColumn] != Piece.NONE) {
-				pieceRow = rowNum;
-			}
-		}
+	public boolean hasWon (int pieceRow, int pieceColumn) {
 		
 		// Get type of the most recently added piece
-		GameHelper.Piece pieceType = board[pieceRow][pieceColumn];
+		Piece pieceType = pieces[pieceRow][pieceColumn];
 		
 		// Check if piece won vertically
-		if (pieceRow < 3 && board[pieceRow + 1][pieceColumn] == pieceType &&
-			                  board[pieceRow + 2][pieceColumn] == pieceType &&
-			                  board[pieceRow + 3][pieceColumn] == pieceType) {
+		if (pieceRow < 3 && pieces[pieceRow + 1][pieceColumn] == pieceType &&
+			                  pieces[pieceRow + 2][pieceColumn] == pieceType &&
+			                  pieces[pieceRow + 3][pieceColumn] == pieceType) {
 			return true;
 		}
 		
 		// Check if piece won horizontally
 		for (int columnNum = 0; columnNum < 4; columnNum++) {
-			if (board[pieceRow][columnNum] == pieceType &&
-				board[pieceRow][columnNum + 1] == pieceType &&
-				board[pieceRow][columnNum + 2] == pieceType &&
-				board[pieceRow][columnNum + 3] == pieceType) {
+			if (pieces[pieceRow][columnNum] == pieceType &&
+				pieces[pieceRow][columnNum + 1] == pieceType &&
+				pieces[pieceRow][columnNum + 2] == pieceType &&
+				pieces[pieceRow][columnNum + 3] == pieceType) {
 				return true;
 			}
 		}
 
 		// Check if piece won diagonally
+		for (int rowNum = 0; rowNum < 3; rowNum++) {
+			for (int columnNum = 0; columnNum < 4; columnNum++) {
+				if (pieces[rowNum][columnNum] == pieceType &&
+					pieces[rowNum + 1][columnNum + 1] == pieceType && 
+					pieces[rowNum + 1][columnNum + 1] == pieceType && 
+					pieces[rowNum + 1][columnNum + 1] == pieceType) {
+						return true;
+					}
+			}
+		}
 		
+		for (int rowNum = 3; rowNum < 6; rowNum++) {
+			for (int columnNum = 0; columnNum < 4; columnNum++) {
+				if (pieces[rowNum][columnNum] == pieceType &&
+					pieces[rowNum - 1][columnNum + 1] == pieceType && 
+					pieces[rowNum - 1][columnNum + 1] == pieceType && 
+					pieces[rowNum - 1][columnNum + 1] == pieceType) {
+						return true;
+					}
+			}
+		}
 		
-		// Return false if the piece didn't result in a win
 		return false;
+	}
+	
+	public Piece[][] getPieces() {
+		return pieces;
 	}
 }
